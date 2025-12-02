7c508d690a789a4382e7d8310adae23a63af7c8e
Mark <mark.irungu@student.moringaschool.com>
2025-12-02 12:32:31 +0300

    Fixed NavBar imports and async tests - all 28 tests passing

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import NavBar from "../components/NavBar";
 
 function ErrorPage() {
   return (
     <>
-      <NavBar />  {/* KEEP this one! ErrorPage needs NavBar since it's outside App */}
       <h1>Oops! Looks like something went wrong.</h1>
     </>
   );
