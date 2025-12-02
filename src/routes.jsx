61b1dbc2d75f75f3d794c29f6d5cfe68474bd874
Mark <mark.irungu@student.moringaschool.com>
2025-12-02 08:40:13 +0300

    first committ

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,27 @@
-
+import Home from "./pages/Home";
+import Actors from "./pages/Actors";
+import Directors from "./pages/Directors";
+import Movie from "./pages/Movie";
+import ErrorPage from "./pages/ErrorPage";
 
 const routes = [
-   
-  ];
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/directors",
+    element: <Directors />
+  },
+  {
+    path: "/actors",
+    element: <Actors />
+  },
+  {
+    path: "/movie/:id",
+    element: <Movie />
+  }
+];
 
 export default routes;
