38d37e77d174e58b6721b39133462875b8747575
Mark <mark.irungu@student.moringaschool.com>
2025-12-02 11:04:25 +0300

    hope

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,3 +1,5 @@
+// src/routes.js
+import App from './App'; // Import the App component
 import Home from "./pages/Home";
 import Actors from "./pages/Actors";
 import Directors from "./pages/Directors";
@@ -7,20 +9,26 @@ import ErrorPage from "./pages/ErrorPage";
 const routes = [
   {
     path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/directors",
-    element: <Directors />
-  },
-  {
-    path: "/actors",
-    element: <Actors />
-  },
-  {
-    path: "/movie/:id",
-    element: <Movie />
+    element: <App />, // This wraps ALL child routes
+    errorElement: <ErrorPage />,
+    children: [ // These are nested inside App
+      {
+        index: true, // Same as path: "/"
+        element: <Home />
+      },
+      {
+        path: "directors", // Note: no leading "/"
+        element: <Directors />
+      },
+      {
+        path: "actors", // Note: no leading "/"
+        element: <Actors />
+      },
+      {
+        path: "movie/:id", // Note: no leading "/"
+        element: <Movie />
+      }
+    ]
   }
 ];
 
